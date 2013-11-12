cfd416974f1d2bcf82f325ceaab0160032859e4c
Joseph Wilson <joseph.s.wilson@gmail.com>
2013-11-12 07:16:39 -0600

    Add user microposts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -24,10 +24,17 @@ module SessionsHelper
     user == current_user
   end
 
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
+  end
+
 	def sign_out
-    	self.current_user = nil
-    	cookies.delete(:remember_token)
-  	end
+    self.current_user = nil
+    cookies.delete(:remember_token)
+  end
 
   def redirect_back_or(default)
     redirect_to(session[:return_to] || default)
